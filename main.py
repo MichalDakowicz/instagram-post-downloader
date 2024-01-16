41070fb9e9e77563fbcde5f82d848b6c65de18a5
Michu <118443526+MichalDakowicz@users.noreply.github.com>
2024-01-16 12:20:47 +0100

    code rewrite

--- main.py
+++ main.py
@@ -2,33 +2,42 @@ import instaloader
 import os
 import glob
 
-def download_post(L, post_url):
-    try:
-        post = instaloader.Post.from_shortcode(L.context, post_url.split("/")[-2])
-        L.download_post(post, target='videos')
-        print("Post downloaded successfully!")
-    except instaloader.exceptions.InstaloaderException as e:
-        print(f"Error: {e}")
 
-L = instaloader.Instaloader(download_pictures=True,
-                            download_videos=True,
-                            download_video_thumbnails=False,
-                            download_geotags=False,
-                            download_comments=False,
-                            save_metadata=False)
 
-login = input("Do you want to login? (sometimes doesn't work without logging in) (yes/no): ")
-if login.lower() == "yes":
-    L.login(input("Username: "), input("Password: "))
-else:
-    print("Skipping login")
+class Downloader:
+    def __init__(self):
+        self.L = instaloader.Instaloader(download_pictures=True,
+                                         download_videos=True,
+                                         download_video_thumbnails=False,
+                                         download_geotags=False,
+                                         download_comments=False,
+                                         save_metadata=False)
+        login = input("Do you want to login? (sometimes doesn't work without logging in) (yes/no): ")
+        if login.lower() == "yes":
+            self.login(input("Username: "), input("Password: "))
+        else:
+            print("Skipping login")
+        self.run()
+    
+    def download_post(self, post_url):
+        try:
+            post = instaloader.Post.from_shortcode(self.L.context, post_url.split("/")[-2])
+            self.L.download_post(post, target='videos')
+            print("Post downloaded successfully!")
+        except instaloader.exceptions.InstaloaderException as e:
+            print(f"Error: {e}")
+    
+    def run(self):
+        print("Starting the download process...")
+        with open("links.txt", "r") as f:
+            post_urls = f.read().splitlines()
+        for i, post_url in enumerate(post_urls):
+            print(f"Downloading post {i+1}/{len(post_urls)}")
+            self.download_post(post_url)
+        for txt_file in glob.glob(".\\videos\\*.txt"):
+            os.remove(txt_file)
 
-print("Starting the download process...")
-with open("links.txt", "r") as f:
-    post_urls = f.read().splitlines()
-for i, post_url in enumerate(post_urls):
-    print(f"Downloading post {i+1}/{len(post_urls)}")
-    download_post(L, post_url)
+download = Downloader()
 
 for txt_file in glob.glob(".\\videos\\*.txt"):
     os.remove(txt_file)
