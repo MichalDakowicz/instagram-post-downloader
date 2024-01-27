4ebb482418117ef6bd47f32adbf7b89fa0d1f54b
Michu <michal.dakowicz@infotech.edu.pl>
2024-01-27 21:26:18 +0100

    big update, fixed the big bulk download, added a quick page copy link chrome extension for testing/use purposes

--- main_flask.py
+++ main_flask.py
@@ -1,3 +1,88 @@
+# from flask import Flask, request, jsonify, render_template, send_from_directory
+# import instaloader
+# import os
+# import glob
+
+# app = Flask(__name__)
+# app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'videos')
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# class Downloader:
+#     def __init__(self, username=None, password=None):
+#         self.L = instaloader.Instaloader(download_pictures=True,
+#                                          download_videos=True,
+#                                          download_video_thumbnails=False,
+#                                          download_geotags=False,
+#                                          download_comments=False,
+#                                          save_metadata=False)
+#         if username and password:
+#             self.login(username, password)
+    
+#     def login(self, username, password):
+#         self.L.login(username, password)
+    
+#     def download_post(self, post_url):
+#         try:
+#             post = instaloader.Post.from_shortcode(self.L.context, post_url.split("/")[-2])
+#             self.L.download_post(post, target='videos')
+#             if post.typename == 'GraphVideo':
+#                 suffix = '.mp4'
+#             else:
+#                 suffix = '.jpg'
+#             return 'videos/' + post.date.isoformat().replace('T', '_').replace(':', '-') + "_UTC" + suffix          
+#         except instaloader.exceptions.InstaloaderException as e:
+#             return False
+    
+#     def run(self, post_urls):
+#         urls = []
+#         for post_url in post_urls:
+#             result = self.download_post(post_url)
+#             if result:
+#                 urls.append(result)
+#         for txt_file in glob.glob(".\\videos\\*.txt"):
+#             os.remove(txt_file)
+#         return urls
+
+# # @app.route('/download', methods=['POST'])
+# # def download():
+# #     username = request.form.get('username')
+# #     password = request.form.get('password')
+# #     post_urls = request.form.get('post_urls').splitlines()
+# #     downloader = Downloader(username, password)
+# #     urls = downloader.run(post_urls)
+# #     return urls
+
+# @app.route('/download', methods=['POST'])
+# def download():
+#     username = request.form.get('username')
+#     password = request.form.get('password')
+#     post_urls = request.form.get('post_urls').splitlines()
+#     downloader = Downloader(username, password)
+#     downloader.run(post_urls)
+#     urls = [os.path.join('videos', os.path.basename(f)) for f in glob.glob(".\\videos\\*")]
+#     return jsonify(urls)
+
+# @app.route('/videos/<path:filename>')
+# def media(filename):
+#     return send_from_directory(
+#         app.config['UPLOAD_FOLDER'],
+#         filename,
+#         as_attachment=True
+#     )
+
+# @app.route('/clear', methods=['POST'])
+# def clear():
+#     files = glob.glob('./videos/*')
+#     for f in files:
+#         os.remove(f)
+#     return jsonify({'status': 'success'})
+
+
+
+
 from flask import Flask, request, jsonify, render_template, send_from_directory
 import instaloader
 import os
@@ -42,18 +127,7 @@ class Downloader:
             result = self.download_post(post_url)
             if result:
                 urls.append(result)
-        for txt_file in glob.glob(".\\videos\\*.txt"):
-            os.remove(txt_file)
         return urls
-
-# @app.route('/download', methods=['POST'])
-# def download():
-#     username = request.form.get('username')
-#     password = request.form.get('password')
-#     post_urls = request.form.get('post_urls').splitlines()
-#     downloader = Downloader(username, password)
-#     urls = downloader.run(post_urls)
-#     return urls
 
 @app.route('/download', methods=['POST'])
 def download():
@@ -61,8 +135,7 @@ def download():
     password = request.form.get('password')
     post_urls = request.form.get('post_urls').splitlines()
     downloader = Downloader(username, password)
-    downloader.run(post_urls)
-    urls = [os.path.join('videos', os.path.basename(f)) for f in glob.glob(".\\videos\\*")]
+    urls = downloader.run(post_urls)
     return jsonify(urls)
 
 @app.route('/videos/<path:filename>')
